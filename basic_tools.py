115a157573add6c5f6fc847f1b9282827488c5cb
Gregory Ashton <ga7g08@soton.ac.uk>
2014-08-06 21:30:01 +0100

    Several updates to basic_tools and structure
    + Moves all CSV data file to common repo and adds USA data
    + Adds method to look up CSV file using keys e.g UK USA
    + Adds rcfile to improve look of plots
    + Adds support for country selection
    + Improves distance time plot
    + Adds velocity distribution plot

--- basic_tools.py
+++ basic_tools.py
@@ -7,6 +7,8 @@ import pandas as pd
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import rc_file
+rc_file("/home/greg/Neutron_star_modelling/matplotlibrc")
 
 DISTANCEMATRIX_BASE_URL = 'http://maps.googleapis.com/maps/api/distancematrix/json'
 
@@ -20,17 +22,29 @@ def GetData(origin, destination, **geo_args):
     url = DISTANCEMATRIX_BASE_URL + '?' + urllib.urlencode(geo_args)
     result = simplejson.load(urllib.urlopen(url))
     data = result['rows'][0]['elements'][0]
+
     duration = float(data['duration']['value'])
     distance = float(data['distance']['value'])
-    v_ave = m_per_s_TO_kmph(distance/duration)
+    v_ave = mps_TO_kmph(distance/duration)
     return duration, distance, v_ave
 
-def m_per_s_TO_kmph(vel_mps):
+def GetCSVFile(key):
+    directory = "./CSV_database/"
+    CSV_dict = {"UK" : directory+"postcodes.csv",
+                "USA" : directory + "zip_code_database.csv"
+               }
+    try:
+        return CSV_dict[key]
+    except KeyError:
+        print "CSV key not in exsistence"
+
+def mps_TO_kmph(vel_mps):
     return vel_mps * 1e-3 * (60.0 * 60.0)
 
 def random_line():
     " Pick random line from file http://stackoverflow.com/questions/3540288/how-do-i-read-a-random-line-from-one-file-in-python"
-    source_file = open("./UK-Postcodes/postcodes.csv")
+    source_file_name = GetCSVFile(args.Country)
+    source_file = open(source_file_name)
     line = next(source_file)
     for num, aline in enumerate(source_file):
       if random.randrange(num + 2): continue
@@ -69,9 +83,19 @@ def UpdateResults(file_name, results):
         df = pd.DataFrame(results, index=[0])
     df.to_csv(file_name, sep=" ")   
 
-def CollectResults(N):
+def CollectResults(N, Country):
+    """ Randomly select N pairs of postcodes from Country and save results
+
+    Parameters
+    ----------
+    N : int
+        Integer number of results two collect
+    Country : str
+        Country code, default is given by the first argument to Country argument
+    """
+
+    results_file = "Results_{}.txt".format(Country)
     for i in range(N):
-        file_name = "Results_UK.txt"
         origin, destination = get_origin_destination()
         try:
             duration_s, distance_m, v_ave_kmph = GetData(origin, destination)
@@ -81,34 +105,62 @@ def CollectResults(N):
                        'distance_m' : distance_m,
                        'v_ave_kmph' : v_ave_kmph
                       }
-            UpdateResults(file_name, results)
+            UpdateResults(results_file, results)
         except KeyError:
             pass
 
-def PlotResults():
-    file_name = "Results_UK.txt"
-    df = pd.read_csv(file_name, sep=" ", skipinitialspace=True)
-    plt.plot(df.duration_s, df.distance_m, "o")
+def PlotDistanceTime(Countries):
+    """Plot the distance against time for Country in Countries """
+    for Country in Countries:
+        results_file = "Results_{}.txt".format(Country)
+        df = pd.read_csv(results_file, sep=" ", skipinitialspace=True)
 
-    speed_limit_mph = 70
-    speed_limit_mps = speed_limit_mph * 1609. / (60 * 60)
-    distances = np.linspace(0, 1e7, 100)
-    plt.plot(distances/speed_limit_mps, distances)
+        print "Average speed = {} kmph".format(
+                mps_TO_kmph(np.average(df.distance_m)/np.average(df.duration_s)))
+        plt.plot(df.duration_s, df.distance_m, "o", alpha=0.3,
+                 label=Country)
+
+        #speed_limit_mph = 70
+        #speed_limit_mps = speed_limit_mph * 1609. / (60 * 60)
+        #distances = np.linspace(0, 1e7, 100)
+        #plt.plot(distances/speed_limit_mps, distances)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Distance [m]")
+    plt.legend(loc=2, frameon=False)
     plt.show()
-    
+
+def PlotVelocities(Countries):
+    """ Plot the velocities of the Countries """
+    for Country in Countries:
+        results_file = "Results_{}.txt".format(Country)
+        df = pd.read_csv(results_file, sep=" ", skipinitialspace=True)
+        plt.hist(df.v_ave_kmph, bins=50, label=Country, alpha=0.5, normed=True)
+
+    plt.xlabel("Velocity [km/h]")
+    plt.ylabel("Normalised count")
+    plt.legend(loc=2, frameon=False)
+    plt.show()
+
 def _setupArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--CollectResults",
+    parser.add_argument("-r", "--CollectResults",
                         action="store_true", help=CollectResults.__doc__)
     parser.add_argument("-N", default=100, type=int,
                         help="Number of points to add to file_name")
-    parser.add_argument("-p", "--PlotResults", 
-                        action="store_true", help=PlotResults.__doc__)
+    parser.add_argument("-p", "--PlotDistanceTime", 
+                        action="store_true", help=PlotDistanceTime.__doc__)
+    parser.add_argument("-v", "--PlotVelocities", help=PlotVelocities.__doc__,
+                        action="store_true")
+    parser.add_argument("-c", "--Country", default="UK", type=str, nargs="*",
+                        help="Country to use datafile from")
+
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = _setupArgs()
     if args.CollectResults:
-        CollectResults(args.N)
-    if args.PlotResults:
-        PlotResults()
+        CollectResults(N=args.N, Country=args.Country)
+    if args.PlotDistanceTime:
+        PlotDistanceTime(Countries=args.Country)
+    if args.PlotVelocities:
+        PlotVelocities(Countries=args.Country)
